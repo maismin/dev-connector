be825c6910949df0e734904a2a5783d023d26124
Simon Mai <maismin@gmail.com>
2019-08-07 12:08:24 -0400

    Connect to MongoDB with Mongoose

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const config = require('./utils/config')
+const connectDB = require('./utils/db')
 
 const app = express()
 
+// Connect Database
+connectDB()
+
 app.get('/', (req, res) => res.send('API Running'))
 
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.listen(config.PORT, () => console.log(`Server started on port ${config.PORT}`))
---
+++ utils/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const config = require('./config')
+const logger = require('./logger')
+
+const db = config.MONGODB_URI
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, { useNewUrlParser: true })
+    logger.info('connected to MongoDB')
+  } catch(err) {
+    logger.error('error connection to MongoDB:', err.message)
+    // Exit process with failure
+    process.exit(1)
+  }
+}
+
+module.exports = connectDB
