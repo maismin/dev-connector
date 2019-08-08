ae72e4878df1ca3a6f950bff47cd1f44dbbfd920
Simon Mai <maismin@gmail.com>
2019-08-07 21:54:04 -0400

    Add backend user authentication / login route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
 const auth = require('../../middleware/auth')
 const logger = require('../../utils/logger')
+const jwt = require('jsonwebtoken')
+const config = require('../../utils/config')
+const { check, validationResult } = require('express-validator')
 
 const User = require('../../models/User')
 
@@ -17,5 +21,63 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('Server error')
   }
 })
+
+// @route   POST api/auth
+// @desc    Authenticate user & get token
+// @access  Public
+router.post(
+  '/', 
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { email, password } = req.body
+
+    try {
+      let user = await User.findOne({ email })
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid credentials' }] })
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password)
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid credentials' }] })
+      }
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+
+      jwt.sign(
+        payload,
+        config.JWTSECRET,
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) {
+            throw err
+          }
+          res.json({ token })
+        }
+      )
+    } catch(err) {
+      logger.error(err.message)
+      res.send(500).send('Server error')
+    }
+  }
+)
 
 module.exports = router
