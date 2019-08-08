b4c137574943f24b66943fb52dfc91aea88d89f9
Simon Mai <maismin@gmail.com>
2019-08-08 11:58:45 -0400

    Fix populate for Get request to current user profile

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -14,7 +14,7 @@ router.get('/me', auth, async (req, res) => {
   try {
     const profile = await Profile
       .findOne({ user: req.user.id })
-      .populate('User', ['name', 'avatar'])
+      .populate('user', ['name', 'avatar'])
 
     if (!profile) {
       return res.status(400).json({ msg: 'There is no profile for this user' })
