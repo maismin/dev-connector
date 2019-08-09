b37e6da2a61dce81edc57e4c49341f1092795183
Simon Mai <maismin@gmail.com>
2019-08-09 15:45:58 -0400

    Create Login form via custom useField hook

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { Link } from 'react-router-dom'
+import { useField } from '../../hooks'
 
 const Login = () => {
-  return <div>Login</div>
+  const email = useField('email')
+  const password = useField('password')
+
+  const onSubmit = e => {
+    e.preventDefault()
+    console.log('SUCCESS')
+  }
+
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Sign Into Your Account
+      </p>
+      <form className="form" onSubmit={e => onSubmit(e)}>
+        <div className="form-group">
+          <input {...email} reset="" placeholder="Email Address" name="email" />
+        </div>
+        <div className="form-group">
+          <input
+            {...password}
+            reset=""
+            placeholder="Password"
+            name="password"
+            minLength="6"
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don&lsquo;t have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </Fragment>
+  )
 }
 
 export default Login
