60ee2c6c8c7520fd108d2f04d1197afaaaade473
Simon Mai <maismin@gmail.com>
2019-08-10 10:16:45 -0400

    Implement edit-profile functionality

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Login from './components/auth/Login'
 import Register from './components/auth/Register'
 import Dashboard from './components/dashboard/Dashboard'
 import CreateProfile from './components/profile-forms/CreateProfile'
+import EditProfile from './components/profile-forms/EditProfile'
 import PrivateRoute from './components/routing/PrivateRoute'
 
 import { loadUser } from './actions/auth'
@@ -40,6 +41,7 @@ const UnconnectedApp = props => {
             path="/create-profile"
             component={CreateProfile}
           />
+          <PrivateRoute exact path="/edit-profile" component={EditProfile} />
         </Switch>
       </section>
     </div>
