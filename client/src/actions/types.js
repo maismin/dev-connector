7feed9255f8500dc37c6139e1a6da810c6836704
Simon Mai <maismin@gmail.com>
2019-08-12 13:39:18 -0400

    Display single post

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -32,6 +32,8 @@ export const ACCOUNT_DELETED = 'ACCOUNT_DELETED'
 
 export const GET_POSTS = 'GET_POSTS'
 
+export const GET_POST = 'GET_POST'
+
 export const ADD_POST = 'ADD_POST'
 
 export const DELETE_POST = 'DELETE_POST'
---
+++ client/src/components/post/Post.js
@@ -0,0 +1,40 @@
+import React, { Fragment, useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import Spinner from '../layout/Spinner'
+import { getPost } from '../../actions/post'
+import PostItem from '../posts/PostItem'
+
+const Post = ({ match, getPost, post: { post, loading } }) => {
+  useEffect(() => {
+    getPost(match.params.id)
+  }, [getPost])
+  return loading || post === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <Link to="/posts" className="btn">
+        Back to Posts
+      </Link>
+      <PostItem post={post} showActions={false} />
+    </Fragment>
+  )
+}
+
+Post.propTypes = {
+  getPost: PropTypes.func.isRequired,
+}
+
+const mapStateToProps = state => ({
+  post: state.post,
+})
+
+const mapDispatchToProps = {
+  getPost,
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Post)
