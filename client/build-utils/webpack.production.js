da86e33c503e9d62e69cece97c512e934614e680
Simon Mai <maismin@gmail.com>
2019-08-12 14:52:54 -0400

    Change webpack production settings

--- client/build-utils/webpack.production.js
+++ client/build-utils/webpack.production.js
@@ -6,6 +6,24 @@ module.exports = () => ({
     filename: '[chunkhash].js',
     publicPath: '/',
   },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 3000,
+    historyApiFallback: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+      },
+      '/profiles/api': {
+        target: 'http://localhost:5000',
+        pathRewrite: { '^/profiles/api': '/api' },
+      },
+      '/posts/api': {
+        target: 'http://localhost:5000',
+        pathRewrite: { '^/posts/api': '/api' },
+      },
+    },
+  },
   plugins: [new CleanWebpackPlugin(), new MiniCssExtractPlugin()],
   module: {
     rules: [
